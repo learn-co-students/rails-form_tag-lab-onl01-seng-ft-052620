8b9b978582479e8bf393714497a2446fc0284bb4
SWFullPotential <swfullpotential@gmail.com>
2020-07-28 18:15:13 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+
+  end
+
+  def create
+    Student.create(first_name: params[:student], last_name: params[:student])
+    redirect_to students_path
+  end
+
+
 end
