49d029813d67957c54dddd26fb5f48399c83229e
lovecosma <williamsmatthew333@gmail.com>
2020-07-30 13:17:21 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,31 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(blog_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+
+  private
+
+  def blog_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
